7414ae3c0130462780c5355b70cae1a28099199e
Gromov Vitaly <gromovitaly@gmail.com>
2020-07-27 15:02:32 +0300

    lesson 7 add toolbar settings action menu

--- app/src/main/java/com/gmail/gromovitaly/telegram_clone/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/gmail/gromovitaly/telegram_clone/ui/fragments/SettingsFragment.kt
@@ -1,5 +1,7 @@
 package com.gmail.gromovitaly.telegram_clone.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.gmail.gromovitaly.telegram_clone.R
 import com.gmail.gromovitaly.telegram_clone.databinding.FragmentSettingsBinding
 
@@ -12,5 +14,11 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true) //вроде как активация меню из правого верхнего угла
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) { //подключаем меню из правого верхнего угла
+        activity?.menuInflater?.inflate(R.menu.menu_settings_action,menu)
+
     }
 }
